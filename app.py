f56caeef625127825dc451b7a86b7877196bb8b6
miniwisdom5999 <miniwisdom@naver.com>
2021-12-07 12:57:11 +0900

    member

---
+++ app.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def getconn():
+    conn = sqlite3.connect('./memberdb.db')
+    return conn
+
+@app.route('/')  #127.0.0.1:5000
+def index():
+    return render_template('index.html')
+    #return "<h1>Welcome~ 방문을 환영합니다.</h1>"
+
+@app.route('/memberlist/')
+def memberlist():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM member"
+    cur.execute(sql)
+    rs = cur.fetchall()
+    conn.close()
+    return render_template('memberlist.html', rs=rs)
+
+@app.route('/memberview/<string:id>/')
+def member_view(id):        #mid를 경로로 설정하고 매개변수로 넘겨줌
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM member WHERE mid = '%s' " % (id)
+    cur.execute(sql)
+    rs = cur.fetchone()     #해당 1개의 자료를 반환받음
+    conn.close()
+    return render_template('member_view.html', rs=rs)
+
+
+
+app.run(debug=True)
