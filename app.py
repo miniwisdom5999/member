ea861aed8f2b744b9f31d6487ba0bd3925590e53
miniwisdom5999 <miniwisdom@naver.com>
2021-12-10 13:00:25 +0900

    register update

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import sqlite3
-
-from flask import Flask, render_template, request, url_for, redirect, session
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = "song"     #암호키 설정
+app.secret_key = "rlarldyd"  #암호키 설정
 
 def getconn():
     conn = sqlite3.connect('./memberdb.db')
@@ -11,8 +10,11 @@ def getconn():
 
 @app.route('/')  #127.0.0.1:5000
 def index():
-    return render_template('index.html')
-    #return "<h1>Welcome~ 방문을 환영합니다.</h1>"
+    if 'userID' in session:   #session에 userID가 존재하면
+        ssid = session.get('userID')  # 세션을 가져옴
+        return render_template('index.html', ssid=ssid) #index로 보내기
+    else:
+        return render_template('index.html')
 
 @app.route('/memberlist/')
 def memberlist():
@@ -20,77 +22,124 @@ def memberlist():
     cur = conn.cursor()
     sql = "SELECT * FROM member"
     cur.execute(sql)
-    rs1 = cur.fetchall()
+    rs = cur.fetchall()   #db에서 검색한 데이터
     conn.close()
-    return render_template('memberlist.html', rs=rs1)
+    if 'userID' in session:  # session에 userID가 존재하면
+        ssid = session.get('userID')  # 세션을 가져옴
+        return render_template('memberlist.html', ssid=ssid, rs=rs)
+    else:
+        return render_template('memberlist.html')
 
-@app.route('/memberview/<string:id>/')
-def member_view(id):        #mid를 경로로 설정하고 매개변수로 넘겨줌
+@app.route('/member_view/<string:id>/')
+def member_view(id):  #mid를 경로로 설정하고 매개변수로 넘겨줌
     conn = getconn()
     cur = conn.cursor()
     sql = "SELECT * FROM member WHERE mid = '%s' " % (id)
     cur.execute(sql)
-    rs = cur.fetchone()     #해당 1개의 자료를 반환받음
+    rs = cur.fetchone()  #해당 1개의 자료를 반환받음
     conn.close()
-    return render_template('member_view.html', rs=rs)
+    if 'userID' in session:  # session에 userID가 존재하면
+        ssid = session.get('userID')  # 세션을 가져옴
+        return render_template('member_view.html', rs=rs, ssid=ssid)
+    else:
+        return render_template('member_view.html')
 
-
-@app.route('/register/', methods = ['GET', 'POST'])    #url경로   ####반드시 대문자로 POST
+@app.route('/register/', methods = ['GET', 'POST'])  #url 경로
 def register():
-    if request.method == 'POST':
-            # 자료 수집
+    if request.method == 'POST':  # 'POST'는 반드시 대문자로 할 것
+        # 자료 수집
+        id = request.form['mid']
+        pwd = request.form['passwd']
+        name = request.form['name']
+        age = request.form['age']
+        #date = request.form['regDate']
+        # 회원 가입
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "INSERT INTO member (mid, passwd, name, age) VALUES ('%s', '%s', '%s', '%s') " \
+             % (id, pwd, name, age)
+        cur.execute(sql)   # 실행 함수
+        conn.commit()      # 커밋 완료
+
+        # 가입 후 자동 로그인
+        sql = "SELECT * FROM member WHERE mid = '%s' " % (id)
+        cur.execute(sql)
+        rs = cur.fetchone()
+        conn.close()
+        if rs:
+            session['userID'] = id   # 자동 로그인시 세션 발급 필수
+            return redirect(url_for('memberlist'))
+    else:
+        return render_template('register.html') #GET 방식
+
+@app.route("/login/", methods = ['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        # 자료 전달 받음
+        id = request.form['mid']
+        pwd = request.form['passwd']
+
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "SELECT * FROM member WHERE mid = '%s' AND passwd = '%s' " % (id, pwd)
+        cur.execute(sql)
+        rs = cur.fetchone() # db에서 찾은 데이터 가져옴
+        conn.close()
+        if rs:
+            session['userID'] = id  # 세션 발급
+            return redirect(url_for('index'))
+        else:
+            error = "아이디나 비밀번호가 일치하지 않습니다."
+            return render_template('login.html', error=error)
+    else:
+        return render_template('login.html')
+
+@app.route('/logout/')
+def logout():
+    #session.pop('userID')  #userID 세션 삭제
+    session.clear()         # 전체의 세션 삭제
+    return redirect(url_for('index'))
+
+@app.route('/member_del/<string:id>/')   #삭제 url 생성
+def member_del(id):  #mid를 매개변수로 넘겨줌
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM member WHERE mid = '%s' " % (id)
+    cur.execute(sql)  # 삭제 수행
+    conn.commit()     # 수행 완료
+    conn.close()
+    return redirect(url_for('memberlist'))
+
+@app.route('/member_edit/<string:id>/', methods = ['GET', 'POST'])
+def member_edit(id):
+    if request.method == "POST":
+        #자료 넘겨 받음
         id = request.form['mid']
         pwd = request.form['passwd']
         name = request.form['name']
         age = request.form['age']
         date = request.form['regDate']
-
+        #db 연결
         conn = getconn()
         cur = conn.cursor()
-        sql = "INSERT INTO member VALUES ('%s', '%s', '%s', '%s', '%s')"\
-            % (id, pwd, name, age, date)
-        cur.execute(sql)    #실행 함수
-        conn.commit()   #커밋완료
-        return redirect(url_for('memberlist'))      #url 결로로 이동
-    else:
-        return render_template('register.html')
-
-
-
-
-
-
-@app.route('/login/', methods = ['GET', 'POST'])
-def login():
-    if request.method == "POST":
-        #자료를 전달받음
-        id = request.form['mid']
-        pwd = request.form['passwd']
-
-        conn = getconn()
-        cur = conn.cursor()
-        sql = "SELECT * FROM member WHERE mid = '%s' and passwd = '%s' " % (id, pwd)
-        cur.execute(sql)
-        rs = cur.fetchone()  #DB에서 찾은 데이터 가져옴   (1개!!)
+        sql = "UPDATE member SET passwd = '%s', name = '%s' , age = '%s', regDate = '%s' " \
+              "WHERE mid = '%s' " % (pwd, name, age, date, id)
+        cur.execute(sql)  # 실행 함수
+        conn.commit()  # 커밋 완료
         conn.close()
-        if rs:
-            session['userID'] = id   # 세션 발급    #로그인하거나..뭐 그럴떄..?
-            return redirect(url_for('index'))
-
-        else:
-            error = "아이디나 비밀번호가 일치하지 않습니다"
-            return render_template('login.html', error=error)
+        return redirect(url_for('member_view', id=id))  # member_view 해당 id로 이동
     else:
-        return render_template('login.html')
-
-
-
-@app.route('/logout')
-def logout():
-    session.pop('userID')   #세션 삭제
-    return redirect(url_for('index'))
-
-
-
+        # 회원 자료 가져오기
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "SELECT * FROM member WHERE mid = '%s' " % (id)
+        cur.execute(sql)
+        rs = cur.fetchone()
+        conn.close()
+        if 'userID' in session:
+            ssid = session.get('userID')
+            return render_template('member_edit.html', rs=rs, ssid=ssid)
+        else:
+            return render_template('member_edit.html')
 
 app.run(debug=True)
--- tbl_member.py
+++ tbl_member.py
@@ -14,10 +14,8 @@ def create_table():
         name TEXT NOT NULL,
         age INTEGER,
         regDate DATETIME DEFAULT CURRENT_TIMESTAMP
-    
     );
     """
-
     cur.execute(sql)
     conn.commit()
     print("member 테이블 생성!!")
@@ -27,7 +25,7 @@ def insert_member():
     conn = getconn()
     cur = conn.cursor()
     sql = "INSERT INTO member(mid, passwd, name, age) VALUES (?, ?, ?, ?)"
-    cur.execute(sql, ('20001', 'm1234', '흥부', 35))
+    cur.execute(sql, ('20001', 'm1234', '흥부', 35 ))
     conn.commit()
     print("멤버 추가")
     conn.close()
@@ -37,16 +35,21 @@ def select_member():
     cur = conn.cursor()
     sql = "SELECT * FROM member"
     cur.execute(sql)
-    rs = cur.fetchall() #DB에서 반환된 자료
-    print(rs)
+    rs = cur.fetchall()   #DB에서 반환된 자료
+    #print(rs)
     for i in rs:
         print(i)
-        #print(i[0])
     conn.close()
 
-
-
+def delete_member():   # 회원 정보 삭제
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM member"
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
 
 #create_table()  #호출
 #insert_member()
+#delete_member()
 select_member()
