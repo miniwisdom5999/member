55aad948d26afa464adc4ff08bcca1013452a521
miniwisdom5999 <miniwisdom@naver.com>
2021-12-14 13:06:09 +0900

    board_view

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ def getconn():
 
 @app.route('/')  #127.0.0.1:5000
 def index():
+    """
     if 'userID' in session:   #session에 userID가 존재하면
         ssid = session.get('userID')  # 세션을 가져옴
         return render_template('index.html', ssid=ssid) #index로 보내기
-    else:
-        return render_template('index.html')
+    else:"""
+    return render_template('index.html')
+
 
 @app.route('/memberlist/')
 def memberlist():
@@ -28,7 +30,7 @@ def memberlist():
         ssid = session.get('userID')  # 세션을 가져옴
         return render_template('memberlist.html', ssid=ssid, rs=rs)
     else:
-        return render_template('memberlist.html')
+        return render_template('memberlist.html', rs=rs)
 
 @app.route('/member_view/<string:id>/')
 def member_view(id):  #mid를 경로로 설정하고 매개변수로 넘겨줌
@@ -38,11 +40,7 @@ def member_view(id):  #mid를 경로로 설정하고 매개변수로 넘겨줌
     cur.execute(sql)
     rs = cur.fetchone()  #해당 1개의 자료를 반환받음
     conn.close()
-    if 'userID' in session:  # session에 userID가 존재하면
-        ssid = session.get('userID')  # 세션을 가져옴
-        return render_template('member_view.html', rs=rs, ssid=ssid)
-    else:
-        return render_template('member_view.html')
+    return render_template('member_view.html')
 
 @app.route('/register/', methods = ['GET', 'POST'])  #url 경로
 def register():
@@ -56,7 +54,7 @@ def register():
         # 회원 가입
         conn = getconn()
         cur = conn.cursor()
-        sql = "INSERT INTO member (mid, passwd, name, age) VALUES ('%s', '%s', '%s', '%s') " \
+        sql = "INSERT INTO member (mid, passwd, name, age) VALUES ('%s', '%s', '%s', %s) " \
              % (id, pwd, name, age)
         cur.execute(sql)   # 실행 함수
         conn.commit()      # 커밋 완료
@@ -118,12 +116,11 @@ def member_edit(id):
         pwd = request.form['passwd']
         name = request.form['name']
         age = request.form['age']
-        date = request.form['regDate']
         #db 연결
         conn = getconn()
         cur = conn.cursor()
-        sql = "UPDATE member SET passwd = '%s', name = '%s' , age = '%s', regDate = '%s' " \
-              "WHERE mid = '%s' " % (pwd, name, age, date, id)
+        sql = "UPDATE member SET passwd = '%s', name = '%s' , age = %s " \
+              "WHERE mid = '%s' " % (pwd, name, age, id)
         cur.execute(sql)  # 실행 함수
         conn.commit()  # 커밋 완료
         conn.close()
@@ -136,10 +133,60 @@ def member_edit(id):
         cur.execute(sql)
         rs = cur.fetchone()
         conn.close()
-        if 'userID' in session:
-            ssid = session.get('userID')
-            return render_template('member_edit.html', rs=rs, ssid=ssid)
-        else:
-            return render_template('member_edit.html')
+        return render_template('member_edit.html', rs=rs)
+
+
+# 게시판 목록
+@app.route('/boardlist/')
+def boardlist():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM board ORDER BY bno DESC"
+    cur.execute(sql)
+    rs = cur.fetchall()
+    print(rs)
+    conn.close()
+    return render_template('boardlist.html', rs=rs)
+
+
+@app.route('/writing/', methods = ['GET', 'POST'])
+def writing():
+    if request.method == "POST":
+        #자료 전달받음
+        title = request.form['title']
+        content = request.form['content']
+        mid = session.get('userID')     #글쓴이 - 로그인한 mid(세션 권한이 있음)
+
+        # db에 글 추가
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "INSERT INTO board(title, content, mid) VALUES ('%s', '%s', '%s')" \
+              % (title, content, mid)
+        cur.execute(sql)
+        conn.commit()
+        print("게시글 추가")
+        conn.close()
+        return redirect(url_for('boardlist'))
+    else:
+        return render_template('writing.html')
+
+
+
+# 게시글 상세보기
+@app.route('/board_view/<int:bno>/')
+def board_view(bno):
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM board WHERE bno = %s" % (bno)
+    cur.execute(sql)
+    rs = cur.fetchone()
+    conn.close()
+    return render_template('board_view.html', rs=rs)
+
+
+
+
+
+
 
 app.run(debug=True)
